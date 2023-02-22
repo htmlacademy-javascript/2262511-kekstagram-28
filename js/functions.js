8c3e448836177daba88892eff50c4c9628ff0116
iazovski <igor.yazovskiy@gmail.com>
2023-02-22 23:02:52 +0800

    delete-comments

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,4 @@
-// Cтрока короче 20 символов
-//имя_функции('проверяемая строка', 20); // true
-// Длина строки ровно 18 символов
-//имя_функции('проверяемая строка', 18); // true
-// Строка длиннее 10 символов
-//имя_функции('проверяемая строка', 10); // false
+
 
 const checkStringLength = (string, length) => string.length <= length;
 
@@ -19,12 +14,6 @@ const isPalindrom = (string) => {
   return reverseString === lowerString;
 };
 
-// Строка является палиндромом
-//имя_функции('топот'); // true
-// Несмотря на разный регистр, тоже палиндром
-//имя_функции('ДовОд'); // true
-// Это не палиндром
-//имя_функции('Кекс');  // false
 
 const wordPalindrom = (string) => {
   const lowerString = string.toLowerCase();
@@ -35,11 +24,7 @@ const wordPalindrom = (string) => {
   return lowerString === reverseString;
 };
 wordPalindrom (' Довод');
-//имя_функции('2023 год');            // 2023
-//имя_функции('ECMAScript 2022');     // 2022
-//имя_функции('1 кефир, 0.5 батона'); // 105
-//имя_функции('агент 007');           // 7
-//имя_функции('а я томат');           // NaN
+
 
 const getNumberFromString = (string) => {
   let result= '';
@@ -55,20 +40,6 @@ const getNumberFromString = (string) => {
    return (result.length > 0) ? Number(result) : NaN;
 }
 
-// Добавочный символ использован один раз
-//имя_функции('1', 2, '0');      // '01'
-
-// Добавочный символ использован три раза
-//имя_функции('1', 4, '0');      // '0001'
-
-// Добавочные символы обрезаны с конца
-//имя_функции('q', 4, 'werty');  // 'werq'
-
-// Добавочные символы использованы полтора раза
-//имя_функции('q', 4, 'we');     // 'wweq'
-
-// Добавочные символы не использованы, исходная строка не изменена
-//имя_функции('qwerty', 4, '0'); // 'qwerty'
 
 const padStart = (string, minLength, extraLine) => {
   if (string.length <= minLength) {
