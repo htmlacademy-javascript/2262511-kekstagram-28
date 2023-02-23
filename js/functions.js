290759a4bd3998aafd30a47ab87f7a2d82e4be44
iazovski <igor.yazovskiy@gmail.com>
2023-02-23 09:46:39 +0800

    fixed-problems-eslint

--- js/functions.js
+++ js/functions.js
@@ -2,64 +2,37 @@
 
 const checkStringLength = (string, length) => string.length <= length;
 
-const isPalindrom = (string) => {
-  const lowerString = string.toLowerCase();
-
-  let reverseString = '';
-
-  for (let i = lowerString.length - 1; i >= 0; i--) {
-    reverseString = reverseString + lowerString[i];
-  }
-
-  return reverseString === lowerString;
-};
-
-
 const wordPalindrom = (string) => {
   const lowerString = string.toLowerCase();
   let reverseString = '';
-  for (let i = lowerString - 1; i >= 0; i --){
+  for (let i = lowerString.length - 1; i >= 0; i --){
     reverseString += lowerString.at(i);
   }
   return lowerString === reverseString;
 };
-wordPalindrom (' Довод');
+wordPalindrom ('Довод');
 
 
 const getNumberFromString = (string) => {
-  let result= '';
-
-   for (let i = 0; i < string.length; i++) {
-     const character = string[i];
-
-      if ((character !== ' ') && !Number.isNaN(+character)) {
-        result = result + character;
-      }
-   }
-
-   return (result.length > 0) ? Number(result) : NaN;
-}
-
-
+  let result = '';
+  for (let i = 0; i < string.length; i++) {
+    const character = string[i];
+    if ((character !== ' ') && !Number.isNaN(+character)) {
+      result = result + character;
+    }
+  }
+  return (result.length > 0) ? Number(result) : NaN;
+};
 const padStart = (string, minLength, extraLine) => {
-  if (string.length <= minLength) {
+  if (string.length >= minLength) {
     return string;
   }
-
   let result = string;
-
   while(result.length < minLength) {
-    // '1', 2, '0'
-    const diffLength = minLength - result.length; // 1
-    const slicedExtraLine = extraLine.slice(0, diffLength); // '0'.slice(0, 1) --> '0'
-    console.log(slicedExtraLine, result)
-    result = slicedExtraLine + result; // 'weq' // 'wweq'
+    const diffLength = minLength - result.length;
+    const slicedExtraLine = extraLine.slice(0, diffLength);
+    result = slicedExtraLine + result;
   }
-
-
-
-  return result;
-  // return pad.slice(0, actualPad % pad.length) + pad.repeat (actualPad/pad.length) + string;
-}
-
+  return string;
+};
 padStart('1', 2, '0');
